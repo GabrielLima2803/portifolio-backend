e28dcfd657262f8643123430b120a156dbbcd505
GabrielLima2803 <gabriellima2803@gmail.com>
2025-03-20 18:21:41 -0300

    fix: Ignorando projects em skills

--- src/main/java/com/lima/portifolio/portfolio/infra/mapper/SkillPersistenceMapper.java
+++ src/main/java/com/lima/portifolio/portfolio/infra/mapper/SkillPersistenceMapper.java
@@ -1,5 +1,8 @@
 package com.lima.portifolio.portfolio.infra.mapper;
 
+import java.util.Set;
+import java.util.stream.Collectors;
+
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.factory.Mappers;
@@ -11,9 +14,18 @@ import com.lima.portifolio.portfolio.infra.entities.SkillJpaEntity;
 public interface SkillPersistenceMapper {
     SkillPersistenceMapper INSTANCE = Mappers.getMapper(SkillPersistenceMapper.class);
 
-    @Mapping(target = "experiences", ignore = true) 
+    @Mapping(target = "experiences", ignore = true)
+    @Mapping(target = "projects", ignore = true) 
     Skill toDomain(SkillJpaEntity skillJpaEntity);
     
-    @Mapping(target = "experiences", ignore = true)  
+    @Mapping(target = "experiences", ignore = true)
+    @Mapping(target = "projects", ignore = true)  
     SkillJpaEntity toEntity(Skill skill);
+
+    default Set<Skill> skillJpaEntitySetToSkillSet(Set<SkillJpaEntity> entities) {
+        if (entities == null) return null;
+        return entities.stream()
+                .map(this::toDomain)
+                .collect(Collectors.toSet());
+    }
 }
