1d3e33b06f767d715a1330bd71710788bfcd4026
GabrielLima2803 <gabriellima2803@gmail.com>
2025-03-20 18:20:51 -0300

    fix: Adição dos @Get e @Post

--- src/main/java/com/lima/portifolio/portfolio/application/controllers/ProjectController.java
+++ src/main/java/com/lima/portifolio/portfolio/application/controllers/ProjectController.java
@@ -4,6 +4,8 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,11 +21,13 @@ public class ProjectController {
     @Autowired
     private ProjectService projectService;
 
+    @PostMapping
     public ResponseEntity<ProjectResponseDTO> createProject(@RequestBody ProjectRequestDTO requestDTO) {
         ProjectResponseDTO responseDTO = projectService.createProject(requestDTO);
         return ResponseEntity.ok(responseDTO);
     }
     
+    @GetMapping
     public ResponseEntity<List<ProjectResponseDTO>> findAllProject() {
         List<ProjectResponseDTO> responseDTOs = projectService.findAllProject();
         return ResponseEntity.ok(responseDTOs);
--- src/main/java/com/lima/portifolio/portfolio/application/controllers/SkillController.java
+++ src/main/java/com/lima/portifolio/portfolio/application/controllers/SkillController.java
@@ -4,6 +4,8 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,11 +21,13 @@ public class SkillController {
     @Autowired
     private SkillService skillService;
 
+    @PostMapping
     public ResponseEntity<SkillResponseDTO> createSkill(@RequestBody SkillRequestDTO requestDTO) {
         SkillResponseDTO responseDTO = skillService.createSkill(requestDTO);
         return ResponseEntity.ok(responseDTO);
     }
 
+    @GetMapping
     public ResponseEntity<List<SkillResponseDTO>> findAllSkill() {
         List<SkillResponseDTO> responseDTOs = skillService.findAllSkill();
         return ResponseEntity.ok(responseDTOs);
