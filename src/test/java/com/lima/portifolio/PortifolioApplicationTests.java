ce56764a7ef83765260393c3c8a07c6a47aad51c
GabrielLima2803 <gabriellima2803@gmail.com>
2025-03-20 23:22:36 -0300

    fix: Configurando class de inicialização de test

--- src/test/java/com/lima/portifolio/PortifolioApplicationTests.java
+++ src/test/java/com/lima/portifolio/PortifolioApplicationTests.java
@@ -1,13 +1,21 @@
 package com.lima.portifolio;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest
+@SpringBootTest(
+	webEnvironment = SpringBootTest.WebEnvironment.MOCK, 
+    classes = { PortifolioApplication.class } 
+)
+@ActiveProfiles("test")
+@AutoConfigureMockMvc
 class PortifolioApplicationTests {
 
 	@Test
-	void contextLoads() {
+	void contextLoads()  throws Exception {
+
 	}
 
 }
